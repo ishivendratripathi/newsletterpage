54a533d633239075b1576e586a198c7044a0b449
Shivendra <shivendra.tripathi@mca.christuniversity.in>
2025-01-21 23:49:13 +0530

    new changes

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,15 +1,14 @@
 import { LogOut } from 'lucide-react'
 import React, { useState } from 'react'
 import { Twitter,SunMedium,CircleHelp } from 'lucide-react'
-
-
+import Logo from './Logo.svg';
 
 const Nav = ({Dark,setDark}) => {
 
 
-  
   return (
     <div className='h-24 flex justify-between w-9/12 m-auto'>
+     <div className='mt-8'><img src={Logo} alt="" /> </div>
     <span className='w-1/4 h-2 p-4'><img src="somthi" alt="" /></span>
     <ul className='flex gap-4'>
       <button onClick={()=>{
